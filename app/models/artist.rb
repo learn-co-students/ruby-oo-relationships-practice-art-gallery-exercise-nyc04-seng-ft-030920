327968a74c3dc80bfff48c5fd7ab6701fee83ee6
Graham Troyer-Joy <graham.troyer-joy@flatironschool.com>
2020-03-16 16:33:39 -0400

    dfshjkdfs

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,32 @@
 class Artist
 
+  @@all = []
+
+  # attr_accessor 
+
   attr_reader :name, :years_experience
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
+  end
+
+  def self.all # class method
+    @@all
+  end
+
+  # Returns an `array` all the paintings by an artist
+  def paintings
+    Painting.all.select {|painting|
+      painting.artist == self 
+    }
+  end
+
+
+  # Returns an `array` of all the galleries that an artist has paintings in
+  def galleries
+
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,7 +1,13 @@
 require_relative '../config/environment.rb'
 
+lee = Artist.new("Lee Bontecou", 30)
+marina = Artist.new("Marina Abramovíc", 25)
 
+untitled_1 = Painting.new("untitled", 999_000, lee, gallerie1)
+untitled_2 = Painting.new("untitled 2", 99_000, marina, gallerie2)
 
+gallerie1 = Gallery.new("Gallerie 1, Paris")
+gallerie2 = Gallery.new("Gallerie 2, Kyoto")
 
 binding.pry
 
