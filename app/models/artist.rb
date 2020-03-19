b3fe20012ca607ca1693fe0575238195e42a718b
replacem3nts <donny.landis@gmail.com>
2020-03-18 21:06:39 -0400

    updates files to meet all deliverables

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,42 @@
 class Artist
-
-  attr_reader :name, :years_experience
+attr_reader :name, :years_experience
+@@all = []
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select {|painting| painting.artist == self}
+  end
+
+  def galleries
+    paintings.map(&:gallery)
+  end
+
+  def cities
+    galleries.map(&:city).uniq
+  end
+
+  def self.total_experience
+    all.sum {|artist| artist.years_experience}
+  end
+
+  def prolificness
+    paintings.count / years_experience
+  end
+
+  def self.most_prolific
+    all.max_by {|artist| artist.prolificness}
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,7 +1,15 @@
 require_relative '../config/environment.rb'
 
+a1 = Artist.new("Andy W", 30)
+a2 = Artist.new("Jeff K", 8)
 
+g1 = Gallery.new("David Zwirner", "New York City")
+g2 = Gallery.new("Beebopaloo", "London")
 
+p1 = Painting.new("untitled colors", 999,  a1, g1)
+p1 = Painting.new("dots dashes", 750,  a1, g1)
+p1 = Painting.new("flannel", 500,  a1, g2)
+p1 = Painting.new("mundane dog", 9000,  a2, g2)
 
 binding.pry
 
